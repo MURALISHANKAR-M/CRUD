2aef65ec959de005c66ca7e269086bce03829c32
MURALISHANKAR.M <theshankar007@gmail.com>
2025-03-02 23:48:39 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const users = require("./sample.json");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+app.use(express.json());
+
+const port = 8000;
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  })
+);
+
+//Display All User
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+//Delete User Detail
+app.delete("/users/:id", (req, res) => {
+  let id = Number(req.params.id);
+  let filteredUsers = users.filter((user) => user.id !== id);
+  fs.writeFile("./sample.json", JSON.stringify(filteredUsers), (err, data) => {
+    return res.json(filteredUsers);
+  });
+});
+
+//Add New User
+app.post("/users", (req, res) => {
+  let { name, age, city } = req.body;
+  if (!name || !age || !city) {
+    res.status(400).send({ message: "All Fields Required" });
+  }
+  let id = Date.now();
+  users.push({ id, name, age, city });
+
+  fs.writeFile("./sample.json", JSON.stringify(filteredUsers), (err, data) => {
+    return res.json({ message: "User Detail Added Success" });
+  });
+});
+
+//Update User
+app.patch("/users/:id", (req, res) => {
+  let id = Number(req.params.id);
+  let { name, age, city } = req.body;
+  if (!name || !age || !city) {
+    res.status(400).send({ message: "All Fields Required" });
+  }
+
+  let index = fs.writeFile(
+    "./sample.json",
+    JSON.stringify(filteredUsers),
+    (err, data) => {
+      return res.json({ message: "User Detail Updated Success" });
+    }
+  );
+});
+
+app.listen(port, (err) => {
+  console.log(`App is running in port ${port}`);
+});
